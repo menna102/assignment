8fbab4f2e4678e1bbf6eaf977e5a780cf1fa0137
menna abdelkader <mennaabdlkader102@gmail.com>
2021-04-20 20:38:06 +0200

    student strucure

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct Student{
+    char* name;
+    int ID;
+    int day,month,year;
+    int scorelastyear;
+};
+
+
+int main()
+{
+    printf("On behalf of all of us, welcome on board\n");
+    return 0;
+}
